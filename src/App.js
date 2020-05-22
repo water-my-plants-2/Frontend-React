9e01a8307b9e118d846f8ab5a04864c9218b33e8
Gabby Venegas <gabbyvenegas@Gabbys-MBP.attlocal.net>
2020-05-22 11:32:59 -0700

    got the funcionality working, still need to finish with the styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
+import { Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <h1 className="header">Plant Parenthood</h1>
+        <div classname="nav-links">
+        <Link to="/">Sign Up</Link>
+        <Link to="/login">Login</Link>
+        </div>
+      </nav>
+      <Route exact path="/">Sign up</Route>
+      <Route path="/login" component={Login}/>
+    
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,126 @@
+import React, { useState, useEffect } from 'react';
+import * as yup from 'yup';
+import axios from 'axios';
+
+
+const formSchema = yup.object().shape({
+    name: yup.string().required("Please enter your username"),
+    password: yup.string()
+    .min(7 , "Must have more that 7 characters")
+    .required("Must enter"),
+});
+
+
+function Login () {
+
+    const [ login, setLogin ] = useState({
+        name:'',
+        password:'',
+    })
+
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+    useEffect(() => {
+    formSchema.isValid(login).then(valid => {
+      setButtonDisabled(!valid);
+    });
+  }, [login]);
+
+
+    const [error , setError] = useState({
+        name:"",
+        password:"",
+    });
+
+    const validate = e => {
+      let value =
+        e.target.type === "checkbox" ? e.target.checked : e.target.value;
+      yup
+        .reach(formSchema, e.target.name)
+        .validate(value)
+        .then(valid => {
+          setError({
+            ...error,
+            [e.target.name]: ""
+          });
+        })
+        .catch(err => {
+          setError({
+            ...error,
+            [e.target.name]: err.errors[0]
+          });
+        });
+      
+    };
+
+      
+    const inputChange = e => {
+      e.persist();
+      validate(e);
+      let value =
+        e.target.type === "checkbox" ? e.target.checked : e.target.value;
+      setLogin({ ...login, [e.target.name]: value });
+    };
+
+
+    const formSubmit = e => {
+      e.preventDefault();
+      console.log("form submitted!");
+      axios
+        .post("https://reqres.in/api/users", login)
+        .then(response => console.log(response))
+        .catch(err => console.log(err));
+        setLogin({
+          name:'',
+          password:'',
+        })
+    };
+
+
+    return(
+        <form onSubmit={formSubmit}>
+           <div className='login'>
+                <h2>Welcome Back!</h2>
+                <p>Log into your account</p>
+
+             
+           <label htmlFor="name">
+               Username
+            <input
+          type="text"
+          name="name"
+          id="name"
+          value={login.name}
+          onChange={inputChange}
+        />
+         {error.name.length > 0 ? (
+          <p className="error">{error.name}</p>
+        ) : null}
+
+           </label>
+           <br/>
+           <label htmlFor="password">
+               Password 
+            <input
+          type="password"
+          name="password"
+          id="password"
+          value={login.password}
+          onChange={inputChange}
+        />
+         {error.password.length > 7 ? (
+          <p className="error">{error.password}</p>
+        ) : null}
+
+           </label>
+           <br/>
+
+           <button disabled={buttonDisabled}>Next</button>
+
+           </div>
+    
+           <pre>{JSON.stringify(Login, null, 2)}</pre>
+        </form>
+    );
+}
+
+export default Login;
